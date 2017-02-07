40a450215ee5cc94a8c0365be94b0ebcefb2837c
rishad_jmr <rishad.k@jmrinfotech.com>
2017-02-07 18:53:53 +0530

    modified usercontroller clas

--- src/main/java/com/jmr/obdx/restcontroller/UserController.java
+++ src/main/java/com/jmr/obdx/restcontroller/UserController.java
@@ -3,6 +3,7 @@ package com.jmr.obdx.restcontroller;
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
